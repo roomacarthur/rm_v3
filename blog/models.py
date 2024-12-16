2d1c7a756f800987d1e8163071241b06d601959d
roomacarthur <roomacarthur@gmail.com>
2024-12-16 17:23:45 +0000

    Adds: blog app, set up of models, templates and views for basic structure

---
+++ blog/models.py
@@ -0,0 +1,50 @@
+from django.db import models
+from django.utils.text import slugify
+from django.urls import reverse
+from django.contrib.auth.models import User
+
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    background_colour = models.CharField(max_length=7, default='#007bff')  # HEX
+    text_color = models.CharField(max_length=100, default='white')
+    slug = models.SlugField(max_length=100, unique=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('blog:category_detail', kwargs={'slug': self.slug})
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    slug = models.SlugField(max_length=100, unique=True)
+    category = models.ForeignKey(Category, on_delete=models.PROTECT)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    feature_image = models.ImageField(upload_to='blog/images', null=True, blank=True)
+    excerpt = models.TextField(max_length=200)
+    content = models.TextField()
+    hashtags = models.CharField(max_length=255, help_text="Comma-separated hashtags")
+    meta_description = models.TextField(max_length=160)
+    meta_keywords = models.CharField(max_length=255, help_text="Comma-separated SEO keywords")
+    alt_text = models.CharField(max_length=100, help_text="Alt text for feature image")
+    created = models.DateTimeField(auto_now_add=True)
+    edited = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = ['-created']
+
+    def __str__(self):
+        return self.title
+
+    def save(self, *args, **kwargs):
+        if not self.slug:  # Auto-generate slug if not provided
+            self.slug = slugify(self.title)
+        super().save(*args, **kwargs)
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', kwargs={'slug': self.slug})
+
+    def get_hashtags_list(self):
+        """Return hashtags as a list of strings."""
+        return [tag.strip() for tag in self.hashtags.split(',') if tag.strip()]
---
+++ blog/views.py
@@ -0,0 +1,26 @@
+from django.views.generic import ListView, DetailView
+from django.http import JsonResponse
+from django.shortcuts import render
+from .models import Post
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/post_list.html'
+    context_object_name = 'posts'
+    paginate_by = 3  # Number of posts to load per request
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['is_htmx'] = self.request.headers.get('HX-Request') == 'true'
+        return context
+
+    def render_to_response(self, context, **response_kwargs):
+        if context['is_htmx']:
+            return render(self.request, 'blog/partials/post_list_partial.html', context, **response_kwargs)
+        return super().render_to_response(context, **response_kwargs)
+
+
+class PostDetailView(DetailView):
+    model = Post
+    template_name = 'blog/post_detail.html'
+    context_object_name = 'post'
