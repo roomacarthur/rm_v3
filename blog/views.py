67854ded3cbcb7fce7bda878b6fc7e02ec530c54
roomacarthur <roomacarthur@gmail.com>
2024-12-16 23:15:34 +0000

    Updates: post list upgrades, sidebar, categories, htmx loading for infinate scroll

--- blog/views.py
+++ blog/views.py
@@ -1,17 +1,18 @@
 from django.views.generic import ListView, DetailView
 from django.http import JsonResponse
 from django.shortcuts import render
-from .models import Post
+from .models import Post, Category
 
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
-    paginate_by = 3  # Number of posts to load per request
+    paginate_by = 5  # Number of posts to load per request
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['is_htmx'] = self.request.headers.get('HX-Request') == 'true'
+        context['categories'] = Category.objects.all()
         return context
 
     def render_to_response(self, context, **response_kwargs):
