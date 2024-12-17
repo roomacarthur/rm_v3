a851edd173b9c6f38a3e06446922704d4f1d6c7a
roomacarthur <roomacarthur@gmail.com>
2024-12-17 22:02:57 +0000

    Updates: blog list template to filter by category and search result

--- blog/views.py
+++ blog/views.py
@@ -1,38 +1,44 @@
 from django.views.generic import ListView, DetailView
-from django.http import JsonResponse
-from django.shortcuts import render
 from .models import Post, Category
+from django.db.models import Q
+
 
 class PostListView(ListView):
     model = Post
     template_name = 'blog/post_list.html'
     context_object_name = 'posts'
-    paginate_by = 3  # Number of posts to load per request
+    paginate_by = 5
 
     def get_queryset(self):
         queryset = Post.objects.all()
         category_slug = self.request.GET.get('category')
+        search_query = self.request.GET.get('search')
+
+        # Filter by category
         if category_slug:
             queryset = queryset.filter(category__slug=category_slug)
+
+        # Search filter
+        if search_query:
+            queryset = queryset.filter(
+                Q(title__icontains=search_query) |
+                Q(hashtags__icontains=search_query) |
+                Q(category__name__icontains=search_query)
+            ).distinct()
+
         return queryset
 
     def get_template_names(self):
         if self.request.htmx:
             return 'blog/partials/post_list_partial.html'
-        
         return 'blog/post_list.html'
     
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['is_htmx'] = self.request.headers.get('HX-Request') == 'true'
         context['categories'] = Category.objects.all()
         context['selected_category'] = self.request.GET.get('category', '')
         return context
-
-    def render_to_response(self, context, **response_kwargs):
-        if context['is_htmx']:
-            return render(self.request, 'blog/partials/post_list_partial.html', context, **response_kwargs)
-        return super().render_to_response(context, **response_kwargs)
+        return context
 
 
 class PostDetailView(DetailView):
