3821cd8e898dacb8d3b065c2001533edfc866353
roomacarthur <roomacarthur@gmail.com>
2024-12-18 19:31:09 +0000

    Adds: DB setup

--- config/settings.py
+++ config/settings.py
@@ -89,7 +89,7 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # }
 
 DATABASES = {
-    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+    'default': dj_database_url.parse(env('DATABASE_URL'))
 }
 
 
