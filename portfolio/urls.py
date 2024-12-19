00dc9077f27148dc7ad8a7a9766a278e712f5ff4
roomacarthur <roomacarthur@gmail.com>
2024-12-19 15:00:04 +0000

    Adds: correct linking in navbar for portfolio, update portfolio listview url name

--- portfolio/urls.py
+++ portfolio/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import PortfolioTemplateView
 
+app_name = 'portfolio'
+
 urlpatterns = [
     path('work/', PortfolioTemplateView.as_view(), name='portfolio'),
 ]
