a26d9d0ddf5ea6927b77951f7d454a67d5e9ea25
roomacarthur <roomacarthur@gmail.com>
2024-12-19 11:14:42 +0000

    Updates: change blog app url from blog to notes

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,6 @@ from .views import PostListView, PostDetailView
 app_name = 'blog'
 
 urlpatterns = [
-    path('blog/', PostListView.as_view(), name='post_list'),
-    path('blog/<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
+    path('notes/', PostListView.as_view(), name='post_list'),
+    path('notes/<slug:slug>/', PostDetailView.as_view(), name='post_detail'),
 ]
