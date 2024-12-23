faa0f90c70d15fbf7c6d9055de3c93816f96988b
roomacarthur <roomacarthur@gmail.com>
2024-12-23 16:47:30 +0000

    Adds: error handling templates

--- config/urls.py
+++ config/urls.py
@@ -1,21 +1,6 @@
-"""
-URL configuration for config project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.1/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
 from django.urls import path, include
+from django.shortcuts import render
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -24,3 +9,18 @@ urlpatterns = [
     path('', include('portfolio.urls')),
     path('summernote/', include('django_summernote.urls')),
 ]
+
+# Custom error handlers
+def custom_404(request, exception):
+    return render(request, 'errors/404.html', status=404)
+
+def custom_500(request):
+    return render(request, 'errors/500.html', status=500)
+
+def custom_403(request, exception):
+    return render(request, 'errors/403.html', status=403)
+
+# Assign custom views to handlers directly
+handler404 = custom_404
+handler500 = custom_500
+handler403 = custom_403
