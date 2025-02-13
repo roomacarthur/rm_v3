572008306b56746ddfd53305d4c540e50c4b751b
roomacarthur <roomacarthur@gmail.com>
2025-02-13 22:43:37 +0000

    Updates: refactor PortfolioDetailView to redirect incomplete projects to the list view

--- portfolio/views.py
+++ portfolio/views.py
@@ -1,15 +1,15 @@
-from django.views.generic import ListView, DetailView
-from .models import PortfolioProject
-from django.http import Http404
+from django.views.generic import DetailView, ListView
+from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import redirect
 from django.urls import reverse
+from .models import PortfolioProject
 
 
 class PortfolioListView(ListView):
     model = PortfolioProject
     template_name = 'portfolio/project_list.html'
     context_object_name = 'projects'
-    paginate_by = 10  # Optional: Paginate the list view
+    paginate_by = 10
 
 
 class PortfolioDetailView(DetailView):
@@ -17,17 +17,8 @@ class PortfolioDetailView(DetailView):
     template_name = 'portfolio/project_detail.html'
     context_object_name = 'project'
 
-
-    def get_object(self, queryset=None):
-        # Retrieve the object (PortfolioProject)
-        obj = super().get_object(queryset)
-        
-        # Check if the project is completed
-        if not obj.is_complete:
-            # Option 1: Redirect to the list view
-            return redirect(reverse('portfolio:portfolio'))  # Redirect to the portfolio list view
-
-            # Option 2: Raise a 404 error if the project isn't completed
-            # raise Http404("Project not found or is not completed.")
-        
-        return obj
+    def get(self, request, *args, **kwargs):
+        self.object = super().get_object()
+        if not self.object.is_complete:
+            return HttpResponseRedirect(reverse('portfolio:portfolio'))
+        return super().get(request, *args, **kwargs)
