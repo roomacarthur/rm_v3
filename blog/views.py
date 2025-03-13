1f7b1dbfc657a159443995badf63a7707004a487
roomacarthur <roomacarthur@gmail.com>
2025-03-13 14:54:57 +0000

    Updates: blog posts lists now paginate by 5

--- blog/views.py
+++ blog/views.py
@@ -10,7 +10,7 @@ class PostListView(ListView):
     model = Post
     template_name = "blog/post_list.html"
     context_object_name = "posts"
-    paginate_by = 3
+    paginate_by = 5
 
     def get_queryset(self):
         """Ensure category & search filters work together properly."""
