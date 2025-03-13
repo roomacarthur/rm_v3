337dc073569f4a276344b70508cb15fe76967d3c
roomacarthur <roomacarthur@gmail.com>
2025-03-13 14:27:03 +0000

    Implement CachedPaginator in portfolio module for optimized post count caching

--- portfolio/views.py
+++ portfolio/views.py
@@ -3,30 +3,9 @@ from django.http import HttpResponseRedirect, Http404
 from django.shortcuts import redirect
 from django.urls import reverse
 from django.core.cache import cache
-from django.core.paginator import Paginator
 
 from .models import PortfolioProject
-
-
-class CachedPaginator(Paginator):
-    """
-    Custom paginator that forces Django to use cached
-    count instead of running COUNT(*)
-    """
-    def __init__(self, object_list, per_page, cache_key, **kwargs):
-        super().__init__(object_list, per_page, **kwargs)
-        self.cache_key = cache_key
-
-    @property
-    def count(self):
-        """Return cached count instead of running COUNT(*) every request."""
-        cached_count = cache.get(self.cache_key)
-        if cached_count is not None:
-            return cached_count
-        # If cache is empty, store the count
-        cached_count = self.object_list.count()
-        cache.set(self.cache_key, cached_count)
-        return cached_count
+from .utils import CachedPaginator
 
 
 class PortfolioListView(ListView):
@@ -58,15 +37,17 @@ class PortfolioListView(ListView):
             per_page,
             cache_key="portfolio_project_count",
             orphans=orphans,
-            allow_empty_first_page=allow_empty_first_page)
+            allow_empty_first_page=allow_empty_first_page
+        )
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
 
+        # Fetch cached project count
         project_count = cache.get("portfolio_project_count")
         if project_count is None:
             project_count = PortfolioProject.objects.count()
-            cache.set("portfolio_project_count", project_count)
+            cache.set("portfolio_project_count", project_count, timeout=86400)  # 24 hours
 
         context["project_count"] = project_count
         return context
